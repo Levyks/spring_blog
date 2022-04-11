27896ca69a74b6a8c343d3588cc2f61348b83f0a
Levy Barbosa <marcio.levy20@gmail.com>
2022-04-11 14:14:40 -0300

    feat: added CORS origins

---
+++ src/main/java/com/levyks/spring_blog/configurations/CorsConfiguration.java
@@ -0,0 +1,17 @@
+package com.levyks.spring_blog.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer
+{
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8000", "https://levy-spring-blog.herokuapp.com");
+    }
+}
